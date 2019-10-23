c88849b3c8b375c83177d384299282fbe9c3475a
beau1992 <blackbeau1992@gmail.com>
2019-10-23 11:56:29 -0600

    images

--- js/library.js
+++ js/library.js
@@ -2,3 +2,10 @@ document.addEventListener("DOMContentLoaded", function(){
     let lastMod = document.lastModified;
     document.getElementById("modDate").innerHTML = lastMod;
     });
+    WebFont.load({
+        google: {
+          families: [
+             'Montserrat'
+          ]
+        }
+      });
