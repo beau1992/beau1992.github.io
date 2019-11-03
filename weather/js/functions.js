1c5b015b486e216f612afad95e3213e443fd76e1
beau1992 <blackbeau1992@gmail.com>
2019-11-03 16:52:55 -0700

    ;alkjdf

--- weather/js/functions.js
+++ weather/js/functions.js
@@ -4,6 +4,9 @@
 *  Weather Site JavaScript Functions
 ************************************* */
 
+// $ = document.querySeLector();
+// $$ = document.querySeLectorALL();
+
 // Listen for the DOM to finish building
 /*document.addEventListener("DOMContentLoaded", function(){
     /*buildModDate();
@@ -26,10 +29,10 @@ function toggleMenu(event){
     navList.classList.toggle("mobileNav");
   }
 
-/*function toggleMenu() {
+function toggleMenu() {
 
     document.getElementById("primaryNav").classList.toggle("hide");
-}*/
+}
 
 //Function for showing the date//
 
@@ -69,8 +72,12 @@ document.addEventListener("DOMContentLoaded", function(){
     let lastMod = document.lastModified;
     document.getElementById("modDate").innerHTML = lastMod;
     // The Time Indictor function
-let hour="6";
+let hour="7";
 timeBall(hour);
+
+//Changing the background
+let currCond= "clear";
+changeSummaryBackground(currCond);
 });
     /*WebFont.load({
         google: {
@@ -115,3 +122,64 @@ function buildWC(speed, temp) {
         item.classList.add("ball");
     }
 }
+
+//Changing the background function
+function changeSummaryBackground(currCond) {
+    console.log(`Value of currCond: ${currCond}`);
+  let condition = (currCond.toLowerCase()).trim(); // standardize input
+  console.log(`Value of condition: ${condition}`);
+  let conditionIndex = 0; // set condition index to 0
+  console.log(`Value of conditionIndex: ${conditionIndex}`);
+  // determine available width
+  let width = window.innerWidth;
+  console.log(`Screen Width: ${width}`);
+   // check if screen is tablet or desktop widths and adjust starting point for condition index
+   if (width > 475 && width <= 800) {
+    conditionIndex += 5;
+    console.log(`Updated value of conditionIndex: ${conditionIndex}`);
+  } else if (width > 800) {
+    conditionIndex += 10
+  }
+
+  // store paths to specific width images
+  const imgURLS = ["/clear400.jpg", "/clouds400.jpg", "/fog400.jpg", "/rain400.jpg", "/snow400.jpg",
+                  "/clear600.jpg", "/clouds600.jpg", "/fog600.jpg", "/rain600.jpg", "/snow600.jpg", 
+                   ];
+  // rest of directory structure
+  const imgDirPrefix = "url(/weather/images";
+  const imgDirPostfix = ")"
+
+  // update condition index based on weather status
+  switch (condition) {
+    case "clear":
+      conditionIndex += 0;
+      break;
+    case "cloudy":
+      conditionIndex += 1;
+      break;
+    case "fog":
+      conditionIndex += 2;
+      break;
+    case "rain":
+      conditionIndex += 3;
+      break;
+    case "snow":
+      conditionIndex += 4;
+      break;
+    default:
+      console.log("Error: Weather type is invalid.")
+      break;
+  }
+  console.log(`Value of conditionIndex: ${conditionIndex}`);
+  
+  // build image url() CSS variable
+  let imageURL = imgDirPrefix + imgURLS[conditionIndex] + imgDirPostfix;
+  console.log(`imageURL is: ${imageURL}`);
+
+  // set background image
+  const backgroundImg = document.body.style; // used to access css
+  // console.log(`backgroundImg: ${backgroundImg}`);
+  backgroundImg.setProperty("--dynamic-weather-background", `${imageURL}`); 
+  console.log(`${imageURL} has been set as the background.`);
+}
+
